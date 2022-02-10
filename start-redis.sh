cc6e7d57a13363a99e7359744cad78161fc9a3a7
Bruno Farias <marcelo.oliveira@opencircle.com.br>
2022-02-10 11:20:58 -0300

    using parameters

--- start-redis.sh
+++ start-redis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 REDIS_VERSION=$1
-REDIS_PORT=$2
+REDIS_PORT_RANGE=$2
 REDIS_CONTAINER_NAME=$3
 
 if [ -z "$REDIS_VERSION" ]; then
@@ -11,4 +11,4 @@ if [ -z "$REDIS_VERSION" ]; then
 fi
 
 echo "Starting single-node Redis instance"
-docker run --name redis-clusterx -e INITIAL_PORT=6379 -e IP=0.0.0.0 --publish 6379-6399:6379-6399 --detach docker.io/grokzen/redis-cluster:latest
+docker run --name $REDIS_CONTAINER_NAME -e INITIAL_PORT=6379 -e IP=0.0.0.0 --publish $REDIS_PORT_RANGE:6379-6399 --detach docker.io/grokzen/redis-cluster:$REDIS_VERSION
